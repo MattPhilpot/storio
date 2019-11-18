571f5473e2d0c7b9cdda8804be6cfba9cd4657b9
mattp <mattp@linuxacademy.com>
2019-11-18 11:53:37 -0600

    Android Studio convert to androidx

--- storio-sample-app/src/main/java/com/pushtorefresh/storio3/sample/ui/activity/BaseActivity.java
+++ storio-sample-app/src/main/java/com/pushtorefresh/storio3/sample/ui/activity/BaseActivity.java
@@ -1,10 +1,10 @@
 package com.pushtorefresh.storio3.sample.ui.activity;
 
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.pushtorefresh.storio3.sample.R;
